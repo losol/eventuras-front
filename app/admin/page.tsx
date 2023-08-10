46dca339644a335b899c9304c1eec1f9a4ec5087
Ole Kristian Losvik <losvik@gmail.com>
2023-08-10 20:45:17 +0200

    feat: Add layout component
    - Add a new layout component to admin
    - Layout adjustments

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,5 +1,6 @@
 import { EventDto, EventsService, UserDto, UsersService } from '@losol/eventuras';
 import { Heading } from 'components/content';
+import { Layout } from 'components/layout';
 import Link from 'next/link';
 
 import AdminEventList from './AdminEventList';
@@ -28,15 +29,17 @@ export default async function AdminPage() {
 
   return (
     <>
-      <Heading as="h1">Admin</Heading>
+      <Layout>
+        <Heading as="h1">Admin</Heading>
 
-      <div className="inline-flex flex-col">
-        <Link href="/admin/users">Users</Link>
-      </div>
+        <div className="inline-flex flex-col">
+          <Link href="/admin/users">Users</Link>
+        </div>
 
-      <Heading as="h2">Arrangement</Heading>
-      <AdminEventList eventinfo={eventinfo} />
-      <UserList users={users} />
+        <Heading as="h2">Arrangement</Heading>
+        <AdminEventList eventinfo={eventinfo} />
+        <UserList users={users} />
+      </Layout>
     </>
   );
 }
--- components/layout/Header.tsx
+++ components/layout/Header.tsx
@@ -1,5 +1,6 @@
+'use client';
+
 import { Button } from 'components/inputs';
-import { UserMenu } from 'components/layout';
 import Link from 'next/link';
 import { signIn, signOut, useSession } from 'next-auth/react';
 import useTranslation from 'next-translate/useTranslation';
@@ -10,7 +11,7 @@ const Header = () => {
 
   return (
     <nav className="bg-gray-200 dark:bg-gray-900 text-gray-800 dark:text-gray-200">
-      <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+      <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto px-4">
         <Link href="/" className="flex items-center">
           <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
             Eventuras
@@ -18,7 +19,7 @@ const Header = () => {
         </Link>
         <div>
           {!session && <Button onClick={() => signIn('auth0')}>{t('header.auth.login')}</Button>}
-          {session && <UserMenu signOut={signOut} name={session.user?.name ?? ''} />}
+          {session && <Button onClick={signOut}>Log out</Button>}
         </div>
       </div>
     </nav>
