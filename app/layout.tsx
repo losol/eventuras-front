bad3b994ef5e4265ad8ab512bee873db9b2ffd67
Ole Kristian Losvik <losvik@gmail.com>
2023-08-12 17:01:33 +0200

    fix: allow unauthenticated front page

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,20 +18,17 @@ export const dynamic = 'force-dynamic';
 
 export default async function RootLayout({ children }: { children: React.ReactNode }) {
   const session: Session | null = await getServerSession(authOptions);
-
-  if (!session) {
-    return <div>No access...</div>;
-  }
-
-  // Create a client side sessionObject without the accessToken
-  const clientSideSession: Session = { ...session };
-  if (clientSideSession.accessToken) {
-    delete clientSideSession.accessToken;
-  }
+  let clientSideSession: Session | null = null;
 
   OpenAPI.BASE = process.env.API_BASE_URL!;
   OpenAPI.VERSION = process.env.NEXT_PUBLIC_API_VERSION!;
-  OpenAPI.TOKEN = session.accessToken ?? '';
+
+  if (session) {
+    clientSideSession = { ...session };
+    delete clientSideSession.accessToken;
+
+    OpenAPI.TOKEN = session.accessToken ?? '';
+  }
 
   return (
     <html lang="en">
