a16915f598ccc97382c5598af164b99f6110fc97
Ole Kristian Losvik <losvik@gmail.com>
2023-08-13 00:38:13 +0200

    Refactor providers and header
    - Refactored the `Providers` component in `app/Providers.tsx` to remove unused imports and unnecessary lines of code.
    - Refactored the `Header` component in `components/layout/Header.tsx` to extract user authentication logic into a separate `UserMenu` component.
    - Created a new `UserMenu` component in `components/layout/UserMenu.tsx` to handle user authentication and display relevant buttons.
    - Removed a console error output in `pages/_app.tsx` and removed the `accessToken` property from the `session` object.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -9,7 +9,11 @@ import { SessionProvider } from 'next-auth/react';
 OpenAPI.BASE = `${process.env.NEXT_PUBLIC_APPLICATION_URL}/api/eventuras`;
 
 export default function App({ Component, pageProps }: AppProps) {
-  console.error('which side?');
+  // Check and remove the accessToken from the session object
+  if (pageProps.session?.accessToken) {
+    delete pageProps.session.accessToken;
+  }
+
   return (
     <SessionProvider session={pageProps.session}>
       <UserProvider>
