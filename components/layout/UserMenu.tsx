974b872566114d64564535370e46f7efee9ebe3a
Ole Kristian Losvik <losvik@gmail.com>
2023-08-13 00:53:28 +0200

    fix: usermenu Spinner

--- components/layout/UserMenu.tsx
+++ components/layout/UserMenu.tsx
@@ -10,16 +10,10 @@ const UserMenu = () => {
   return (
     <>
       {status === 'unauthenticated' && <Button onClick={() => signIn('auth0')}>Log in</Button>}
-      {status === 'authenticated' && (
-        <Button onClick={() => signOut()}>
-          <Spinner />
-          Log out
-        </Button>
-      )}
+      {status === 'authenticated' && <Button onClick={() => signOut()}>Log out</Button>}
       {status === 'loading' && (
         <Button className="cursor-not-allowed" disabled={true}>
           <Spinner />
-          Loading
         </Button>
       )}
     </>
