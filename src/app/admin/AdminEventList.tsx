5fe3a1de6f845eb844343e6fe6f18243ee70ac4e
Meinte Van't Kruis <meinte@gmail.com>
2023-09-26 19:27:09 +0200

    feat: added action to send email with included markdown editor (todo: parse markdown, api integration)

--- src/app/admin/AdminEventList.tsx
+++ src/app/admin/AdminEventList.tsx
@@ -1,33 +1,49 @@
 'use client';
 
 import { EventDto } from '@losol/eventuras';
+import { IconMailForward } from '@tabler/icons-react';
 import Link from 'next/link';
+import { useState } from 'react';
 
 import { createColumnHelper, DataTable } from '@/components/content';
+import MarkdownEditor from '@/components/editors/MarkdownEditor';
+import Button, { blueBlockClasses } from '@/components/inputs/Button';
+import { Drawer } from '@/components/layout';
 const columnHelper = createColumnHelper<EventDto>();
-
-const columns = [
-  columnHelper.accessor('title', {
-    header: 'Title',
-    cell: info => (
-      <Link href={`/admin/events/${info.row.original.id}/edit`}> {info.getValue()}</Link>
-    ),
-  }),
-  columnHelper.accessor('location', {
-    header: 'Location',
-    cell: info => info.getValue(),
-  }),
-  columnHelper.accessor('dateStart', {
-    header: 'When',
-    cell: info => info.getValue(),
-  }),
-];
-
 interface AdminEventListProps {
   eventinfo: EventDto[];
 }
 
 const AdminEventList: React.FC<AdminEventListProps> = ({ eventinfo = [] }) => {
+  const [drawerIsOpen, setDrawerIsOpen] = useState(false);
+
+  const actions = (
+    <>
+      <IconMailForward className="cursor-pointer" onClick={() => setDrawerIsOpen(true)} />
+    </>
+  );
+
+  const columns = [
+    columnHelper.accessor('title', {
+      header: 'Title',
+      cell: info => (
+        <Link href={`/admin/events/${info.row.original.id}/edit`}> {info.getValue()}</Link>
+      ),
+    }),
+    columnHelper.accessor('location', {
+      header: 'Location',
+      cell: info => info.getValue(),
+    }),
+    columnHelper.accessor('dateStart', {
+      header: 'When',
+      cell: info => info.getValue(),
+    }),
+    columnHelper.accessor('action', {
+      header: 'Action',
+      cell: () => actions,
+    }),
+  ];
+
   return (
     <>
       <DataTable
@@ -36,6 +52,21 @@ const AdminEventList: React.FC<AdminEventListProps> = ({ eventinfo = [] }) => {
         clientsidePaginationPageSize={250}
         clientsidePagination
       />
+      <Drawer isOpen={drawerIsOpen} onCancel={() => setDrawerIsOpen(false)}>
+        <Drawer.Header className="text-black">
+          Send Notification Email to Participants
+        </Drawer.Header>
+        <Drawer.Body>
+          <MarkdownEditor />
+          <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto px-4">
+            <Button className={`flex-auto justify-center m-1 ${blueBlockClasses}`}>Send</Button>
+            <Button className={`flex-auto justify-center m-1m ${blueBlockClasses}`}>Cancel</Button>
+          </div>
+        </Drawer.Body>
+        <Drawer.Footer>
+          <></>
+        </Drawer.Footer>
+      </Drawer>
     </>
   );
 };
