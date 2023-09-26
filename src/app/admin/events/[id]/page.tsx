5f39ee0f0da4ba1b7766df118920b00fd27899ff
Ole Kristian Losvik <losvik@gmail.com>
2023-09-26 19:32:04 +0200

    fix: check that events exist

--- src/app/admin/events/[id]/page.tsx
+++ src/app/admin/events/[id]/page.tsx
@@ -4,12 +4,25 @@ import EventEditor from '@/app/admin/events/EventEditor';
 import { Loading } from '@/components/feedback';
 import { useEvent } from '@/hooks/apiHooks';
 
-const EditEvent = ({ params }: { params: any }) => {
-  const eventId = parseInt(params.id as string, 10);
-  const { loading, event } = useEvent(eventId);
+type EditEventinfoProps = {
+  params: {
+    id: string;
+  };
+};
+
+const EditEventinfo: React.FC<EditEventinfoProps> = ({ params }) => {
+  const eventId = parseInt(params.id, 10);
+  const { loading, event: eventinfo } = useEvent(eventId);
+
   if (loading) {
     return <Loading />;
   }
-  return <EventEditor eventinfo={event} />;
+
+  if (!eventinfo) {
+    return <div>Event not found</div>;
+  }
+
+  return <EventEditor eventinfo={eventinfo} />;
 };
-export default EditEvent;
+
+export default EditEventinfo;
